3f6f780e9f0fa3ed4bbb9afc9168c75f590d9d98
ibrahimkpsz <ibrahimkpsz@outlook.com>
2024-12-07 17:39:40 +0300

    first commit

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,10 +8,8 @@ const routes = require("./routes");
 
 const app = express();
 
-app.use(cors({
-    origin: process.env.FRONTEND_URL,
-    credentials: true,
-}));
+app.use(cors({ origin: "https://next-portfoliofrontend.vercel.app/" }));
+
 app.use(bodyParser());
 
 app.use((err, req, res, next) => {
